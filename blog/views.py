ded7786ac9d8c1ab210b3e5a186b161e1547c04c
nsyler <nsyler15@gmail.com>
2023-06-10 13:49:56 -0400

    Made the pages more dynamic

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,81 @@
+from datetime import date
+
 from django.shortcuts import render
 
+def get_date(post):
+    return post['date']
+
+all_posts = [
+    {
+        'slug': 'hike-in-the-mountains',
+        'image': 'mountains.jpeg',
+        'author': 'Noah',
+        'date': date(2023,6,10),
+        'title': 'Mountain Hiking',
+        'excerpt': 'Views while hiking are great',
+        'content': '''Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Commodi explicabo facilis, nemo beatae exercitationem error harum omnis consectetur, soluta odit quidem laudantium blanditiis maiores necessitatibus natus! 
+        Repudiandae in ducimus eum?'''
+
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpeg",
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpeg",
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
+
 def starting_page(request):
-    return render(request, 'blog/index.html')
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, 'blog/index.html', {
+        'posts' : latest_posts
+        })
 
 def posts(request):
-    return render(request, 'blog/all-posts.html')
+    return render(request, 'blog/all-posts.html', {
+        'all_posts': all_posts
+    })
 
 def post_detail(request, slug):
-    return render(request, 'blog/post-detail.html')
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, 'blog/post-detail.html', {
+        'post': identified_post
+    })
