8f5c04d064fcbcd50c116ccc99c66b12a7e056b8
nsyler <nsyler15@gmail.com>
2023-06-16 10:54:41 -0400

    previous comments are now displayed

--- blog/views.py
+++ blog/views.py
@@ -38,7 +38,8 @@ class SinglePostView(View):
         context = {
             'post': post,
             'post_tags': post.tags.all(),
-            'comment_form': CommentForm()
+            'comment_form': CommentForm(),
+            'comments': post.comments.all().order_by('-id')
         }
         return render(request, 'blog/post-detail.html', context)
 
@@ -55,7 +56,8 @@ class SinglePostView(View):
             context = {
             'post': post,
             'post_tags': post.tags.all(),
-            'comment_form': CommentForm()
+            'comment_form': CommentForm(),
+            'comments': post.comments.all().order_by('-id')
             }
             return render(request, 'blog/post-detail.html', context)
 
